84c538286943ecb0e4c74548070493fd70fd9c6f
Diogo Marques <diogo@fontainhas.com>
2019-11-13 15:46:14 +0000

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,8 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -16,6 +18,26 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {beer: beers[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 
 app.listen(3000);
